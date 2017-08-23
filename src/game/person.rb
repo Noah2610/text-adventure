9bc01c68f6b4b9b82828a64eca4b89847313ef35
Noah Rosenzweig <noah@rays-lab.com>
2017-08-23 16:55:29 +0200

    Added gibberish talk output for aliens

--- src/game/person.rb
+++ src/game/person.rb
@@ -18,9 +18,32 @@ end
 class Aliens_abduct_person < Person
 	def init
 	end
-
-	def talk
+	def start_talk
 		return @text[:talk]
+	end
+	def talk (meth=false,params=[])
+		return start_talk  unless meth
+		return "#@name:\n" + gibberish
+	end
+	def gibberish
+		lines = rand(1..4)
+		ret = []
+		lines.times do
+			arr = ("!".."z").to_a.shuffle[0..rand(20..60)]
+			skip = false
+			arr.map! do |a|
+				if (!skip) && (rand(0...5) == 0)
+					skip = true
+					next " "
+				else
+					skip = false
+					skip = true  if (a == " ")
+					next a
+				end
+			end
+			ret.push arr.join("")
+		end
+		return ret.map{ |a| next a unless (a[0] == " "); a[0] = ("!".."z").to_a.sample; next a }.join("\n")
 	end
 end
 
